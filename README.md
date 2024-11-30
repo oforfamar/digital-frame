e46c070c5ce23c834c94b5c77deb34152e0e1e3f
Tudor Roman <tudor.roman22@gmail.com>
2024-11-30 13:14:17 +0200

    add install script and readme instructions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # digital-frame
+
 My DIY digital frame
+
+## Installation
+
+Execute the command below in a terminal to install the slideshow
+
+```bash
+wget -O - https://raw.githubusercontent.com/oforfamar/digital-frame/blob/main/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CURRENT_USER=$(whoami)
+
+echo "Upgrading packages and installing feh"
+sudo apt update && sudo apt upgrade -y && sudo apt install feh -y
+
+echo "Creating the target directory and downloading the slideshow script"
+
+# Create the target directory if it doesn't exist
+mkdir -p /home/$CURRENT_USER/Documents/slideshow
+
+# Download start.sh to the target directory
+wget -O /home/$CURRENT_USER/Documents/slideshow/start.sh https://raw.githubusercontent.com/oforfamar/digital-frame/blob/main/start.sh
+
+# Make the downloaded script executable
+chmod +x /home/$CURRENT_USER/Documents/slideshow/start.sh
+
+echo "Creating the autostart .desktop file for the current user"
+
+# Create the target directory if it doesn't exist
+mkdir -p /home/$CURRENT_USER/.config/autostart
+
+# Change to the target directory
+cd /home/$CURRENT_USER/.config/autostart
+
+# Create the .desktop file
+touch slideshow.desktop
+
+# Write the contents of the .desktop file
+cat <<EOT >> slideshow.desktop
+[Desktop Entry]
+Type=Application
+Name=Slideshow
+Comment=Start picture slideshow on login
+Exec=/home/$CURRENT_USER/Documents/slideshow/start.sh
+NoDisplay=false
+EOT
+
+echo "Install finished. Please reboot to see the slideshow in action."
